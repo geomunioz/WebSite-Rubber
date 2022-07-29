b1fa59d33f819c8af0301bccae56452105f7f5a6
jorgetmz <jorgetmz@outlook.com>
2022-07-28 22:08:28 -0500

    Despligeue de pagina Diseño

--- js/carousel.js
+++ js/carousel.js
@@ -29,3 +29,24 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+var swiper5 = new Swiper(".mySwiper5", {
+    grabCursor: true,
+    effect: "creative",
+    creativeEffect: {
+      prev: {
+        shadow: true,
+        translate: ["-100%", 0, -800],
+        rotate: [0, 0, -90],
+      },
+      next: {
+        shadow: true,
+        translate: ["125%", 0, -800],
+        rotate: [0, 0, 0],
+      },
+    },
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+  });
